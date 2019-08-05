758ff487b82b7cd51417f19fd617a1b29577f728
--global <aaron@aaronschlegel.me>
2019-08-05 07:49:15 -0700

    commit changes to tests

--- tests/testthat/test-authentication.R
+++ tests/testthat/test-authentication.R
@@ -5,6 +5,8 @@ httptest::with_mock_api({
   test_that('authentication to Petfinder API works correctly', {
     skip_on_cran()
     pf <- Petfinder(key = Sys.getenv('PETFINDER_KEY'), 
-                    secret = Sys.getenv('PETFINDER_SECRET'))
+                    secret = Sys.getenv('PETFINDER_SECRET_KEY'))
+    
+    expect_true(is.character(pf$.__enclos_env__$private$auth))
   })
 })
--- tests/testthat/test-methoddocs.R
+++ tests/testthat/test-methoddocs.R
@@ -1,8 +1,8 @@
 context('dummy test for method doc functions')
 
 test_that("dummy methods don't do anything", {
-  # expect_null(breeds('cat'))
-  # expect_null(animal_types('cat'))
-  # expect_null(animals())
-  # expect_null(organizations())
+  expect_null(breeds('cat'))
+  expect_null(animal_types('cat'))
+  expect_null(animals())
+  expect_null(organizations())
 })
