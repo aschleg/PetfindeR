fc190fe37ae0475ace8847c9d5d2bd2bb1cb03ba
--global <aaron@aaronschlegel.me>
2020-07-03 09:08:48 -0700

    test update

--- tests/testthat/test-animals.R
+++ tests/testthat/test-animals.R
@@ -37,7 +37,7 @@ test_that('animals method returns correct information', {
   expect_true(all(good_with_filters$environment.dogs))
   
   before_date <- pf$animals(before_date = '2020-06-29')
-  expect_true(all(before_date$page1$published_at >= lubridate::format_ISO8601(as.POSIXct('2020-06-29 00:00:00'), usetz = TRUE)))
+  expect_true(all(before_date$page1$published_at < lubridate::format_ISO8601(as.POSIXct('2020-06-29'), usetz = TRUE)))
   
   after_date <- pf$animals(after_date = '2020-06-30 0:0:0')
   expect_true(all(after_date$page1$published_at >= lubridate::format_ISO8601(as.POSIXct('2020-06-30'), usetz = TRUE)))
